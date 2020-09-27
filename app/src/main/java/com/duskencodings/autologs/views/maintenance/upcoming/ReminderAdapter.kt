e22d0c73debd0b5987206b069ba8436999f80abe
Tyler Howes <tylerkhowes@gmail.com>
2020-09-27 16:07:37 -0600

    clean up reminder priority/color logic

--- app/src/main/java/com/duskencodings/autologs/views/maintenance/upcoming/ReminderAdapter.kt
+++ app/src/main/java/com/duskencodings/autologs/views/maintenance/upcoming/ReminderAdapter.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.CallSuper
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.duskencodings.autologs.R
 import com.duskencodings.autologs.models.Reminder
 import com.duskencodings.autologs.models.ReminderType
 import com.duskencodings.autologs.utils.clearAndAdd
 import com.duskencodings.autologs.utils.formatted
+import com.duskencodings.autologs.utils.now
 import com.duskencodings.autologs.utils.setTextOrHide
 import kotlinx.android.synthetic.main.maintenance_reminder.view.*
 
@@ -43,12 +46,16 @@ class ReminderAdapter(context: Context,
   }
 
   open inner class ReminderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-
     @CallSuper
     open fun bind(reminder: Reminder) {
       itemView.apply {
+        background = when {
+          reminder.expireAtDate.isAfter(now().plusDays(7)) -> null
+          reminder.expireAtDate.isAfter(now()) -> ResourcesCompat.getDrawable(resources, R.drawable.yellow_tint_bordered_rect, null)
+          else -> ResourcesCompat.getDrawable(resources, R.drawable.red_tint_bordered_rect, null)
+        }
         name.text = reminder.name
-        due_on_miles.text = "${reminder.expireAtMiles} miles"
+        due_on_miles.text = context.getString(R.string.x_miles, reminder.expireAtMiles)
         description.setTextOrHide(reminder.description)
         due_on_date.setTextOrHide(reminder.expireAtDate.formatted())
 
